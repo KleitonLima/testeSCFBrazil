1cd6d0c26dced02b27fbbca4f1e605798b3ae3ce
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-06-13 22:50:37 -0300

    feat(fakeData): adicionado propriedade readings

--- fakeData.js
+++ fakeData.js
@@ -1,11 +1,10 @@
-
-const fakeData  =  [
-    {
-        id: 1,
-        name: "João Oliveira",
-        job: "Desenvolvedor"
-    }
-]
-
+const fakeData = [
+  {
+    id: 1,
+    name: "João Oliveira",
+    job: "Desenvolvedor",
+    readings: 0,
+  },
+];
 
 module.exports = fakeData;
