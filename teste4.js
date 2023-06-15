297b269b2aad3116e3bc5712b9a41da9814c067a
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-06-15 01:18:30 -0300

    feat(teste4): verificação de existência adicionada

--- teste4.js
+++ teste4.js
@@ -4,7 +4,12 @@ module.exports = function (req, res) {
   const id = req.query.id;
 
   const reg = data.find((e) => e.id == id);
+  const name = req.body.name;
+  const job = req.body.job;
+
   reg
-    ? ((reg.name = req.body.name), (reg.job = req.body.job), res.send(reg))
+    ? name && job
+      ? ((reg.name = name), (reg.job = job), res.send(reg))
+      : res.status(404).send("Não é possível atualizar os dados para vazio!")
     : res.status(404).send("Usuário não encontrado!");
 };
