bd0e8c5c454c7d0b641ceecea94d4b25d34d049f
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-06-14 23:54:42 -0300

    feat(middleware): add checkAdminMiddleware

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,6 @@
+const data = require("./fakeData");
+const simplificar = require("./utils");
+
 const checkEspacoMiddleware = (req, res, next) => {
   const name = req.query.name;
 
@@ -8,6 +11,21 @@ const checkEspacoMiddleware = (req, res, next) => {
   next();
 };
 
+const checkAdminMiddleware = (req, res, next) => {
+  const id = req.query.id;
+
+  const user = data.find((e) => {
+    return e.id == id;
+  });
+
+  if (user.admin === false) {
+    return res.send("Usuário não autorizado!");
+  }
+
+  next();
+};
+
 module.exports = {
   checkEspacoMiddleware,
+  checkAdminMiddleware,
 };
