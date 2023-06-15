060d6e524b6c2b5ae480c4b6d4f9fd1f7168c31c
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-06-14 23:10:50 -0300

    deete(teste1):  função simplificar transferida

--- teste1.js
+++ teste1.js
@@ -1,27 +1,12 @@
 const data = require("./fakeData");
+const simplificar = require("./utils");
 
 const getUser = (req, res) => {
   const name = req.query.name;
 
-  // #Forma mais simples
-
   const user = data.find((e) => {
-    return e.name == name;
+    return simplificar(e.name) == simplificar(name);
   });
-
-  // #Forma mais completa aplicando filtros na pesquisa
-
-  //   // função para retirar acentos e espaços e deixar tudo minúsculo
-  // const simplificar = (element) => {
-  //   return element
-  //     .normalize("NFD")
-  //     .replace(/[\u0300-\u036f]/g, "")
-  //     .toLowerCase();
-  // };
-  
-  // const user = data.find((e) => {
-  //   return simplificar(e.name) == simplificar(checkEspaco);
-  // });
 
   user ? res.send(user) : res.send("Usuário não encontrado");
 };
